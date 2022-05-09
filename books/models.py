931005e93dbee219a43b3f546341bd97f14743b9
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-09 17:12:27 -0400

    almost done with goal 1 and goal 2

--- books/models.py
+++ books/models.py
@@ -8,3 +8,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    url = models.URLField(blank=True, null=True)
--- django_freeshelf/settings.py
+++ django_freeshelf/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "registration",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -115,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "US/Eastern"
 
 USE_I18N = True
 
